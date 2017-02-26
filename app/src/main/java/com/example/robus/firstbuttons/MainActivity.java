f07e1f5ae15dd73bd6fb3b258cc7836f5fe6332b
robus <robus@example.com>
2017-02-26 19:46:42 +0100

    fist OnClickListener msg

--- app/src/main/java/com/example/robus/firstbuttons/MainActivity.java
+++ app/src/main/java/com/example/robus/firstbuttons/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.robus.firstbuttons;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button robusButton = (Button) findViewById(R.id.rubusFindBtn);
+        robusButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        TextView robusResult = (TextView) findViewById(R.id.robusResulTxtVie);
+                        robusResult.setText("Good job boss");
+                    }
+                }
+        );
     }
 }
